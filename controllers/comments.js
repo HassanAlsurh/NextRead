3783c75412b9d74da13c466f88bfd51bcd433686
Hasan Ali <hassanalsurh@gmail.com>
2026-07-22 09:11:15 +0300

    Comments CRUD complete with error handling

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,9 +5,30 @@ const Comment = require("../models/comment");
 const newComment = async (req, res) => {
   try {
     let toUpload = {};
-    toUpload.comment = req.body.commentContent;
-    toUpload.userId = req.session.user.id;
-    toUpload.bookId = req.params.bookId;
+
+    if (req.body.commentContent) {
+      toUpload.comment = req.body.commentContent;
+    } else {
+      return res.render("error.ejs", {
+        msg: "Comment cannot be empty",
+      });
+    }
+
+    if (req.session) {
+      toUpload.userId = req.session.user.id;
+    } else {
+      return res.render("error.ejs", {
+        msg: "Please log in to comment",
+      });
+    }
+
+    if (req.params.bookId) {
+      toUpload.bookId = req.params.bookId;
+    } else {
+      return res.render("error.ejs", {
+        msg: "An error happened, please refresh the page!",
+      });
+    }
 
     await Comment.create(toUpload);
     res.redirect(`/books/${req.params.bookId}`);
@@ -16,7 +37,26 @@ const newComment = async (req, res) => {
   }
 };
 
-const editComment = async (req, res) => {};
+const editComment = async (req, res) => {
+  try {
+    const commentToEdit = await Comment.findById(req.params.commentId);
+
+    if (req.body.commentContent) {
+      commentToEdit.comment = req.body.commentContent;
+    } else {
+      return res.render("error.ejs", {
+        msg: "Comment cannot be empty",
+      });
+    }
+
+    commentToEdit.save();
+
+    res.redirect(`/books/${req.params.bookId}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const deleteComment = async (req, res) => {
   try {
     console.log(req.params.commentId);
